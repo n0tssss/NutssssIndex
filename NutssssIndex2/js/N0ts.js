485f77947d581f44d1aaae816dff3fc525de2e9d
N0ts <mail@n0ts.cn>
2021-12-03 11:11:56 +0800

    ♻️[Refactoring]: nutssssIndex2 使用 vue 重构完成

--- NutssssIndex2/js/N0ts.js
+++ NutssssIndex2/js/N0ts.js
@@ -1,7 +1,41 @@
+/*
+ * @Author: N0ts
+ * @Date: 2021-11-16 16:59:53
+ * @LastEditTime: 2021-12-03 10:34:54
+ * @Description: main
+ * @FilePath: /NutssssIndex2/js/N0ts.js
+ * @Mail：mail@n0ts.cn
+ */
+
+import c from "./config.js";
+
 window.onload = function () {
     document.querySelector(".load").style.display = "none";
     document.querySelector(".Box").classList.add("BoxLoad");
 };
+
+let seconds = 1000;
+let minutes = seconds * 60;
+let hours = minutes * 60;
+let days = hours * 24;
+let years = days * 365;
+let today = new Date();
+let todayYear = today.getFullYear();
+let todayMonth = today.getMonth() + 1;
+let todayDate = today.getDate();
+let todayHour = today.getHours();
+let todayMinute = today.getMinutes();
+let todaySecond = today.getSeconds();
+let t1 = c.loveTime;
+let t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
+let diff = t2 - t1;
+let diffYears = Math.floor(diff / years);
+let diffDays = Math.floor(diff / days - diffYears * 365);
+let diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
+let diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
+let diffSeconds = Math.floor(
+    (diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds
+);
 
 new Vue({
     el: "#app",
@@ -10,13 +44,24 @@ new Vue({
         menuTabState: false, // 侧边栏顶部菜单是否展开
         pagesTabState: false, // 侧边栏页面菜单是否展开
         pageIndex: 0, // 当前页面索引
-        timeText: "" // 计时文案
+        timeText: "", // 计时文案
+        giteeData: null, // 码云仓库数据
     },
-    created() {},
+    created() {
+        // 获取 Gitee 仓库信息
+        this.getGiteeData();
+    },
     mounted() {
+        // 计时器开始
         setInterval(() => {
             this.lovetime();
         }, 1000);
+
+        // 图片查看
+        const imgs = document.querySelectorAll(".pagesTextBox img");
+        imgs.forEach((item) => {
+            new Viewer(item);
+        });
     },
     methods: {
         /**
@@ -55,41 +100,37 @@ new Vue({
             this.pageIndex = i;
         },
 
+        /**
+         * 时差计算
+         */
         lovetime() {
-            let seconds = 1000;
-            let minutes = seconds * 60;
-            let hours = minutes * 60;
-            let days = hours * 24;
-            let years = days * 365;
-            let today = new Date();
-            let todayYear = today.getFullYear();
-            let todayMonth = today.getMonth() + 1;
-            let todayDate = today.getDate();
-            let todayHour = today.getHours();
-            let todayMinute = today.getMinutes();
-            let todaySecond = today.getSeconds();
-            let t1 = Date.UTC(2018, 11, 19, 00, 00, 00);
-            let t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
-            let diff = t2 - t1;
-            let diffYears = Math.floor(diff / years);
-            let diffDays = Math.floor(diff / days - diffYears * 365);
-            let diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
-            let diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
-            let diffSeconds = Math.floor(
+            today = new Date();
+            todayYear = today.getFullYear();
+            todayMonth = today.getMonth() + 1;
+            todayDate = today.getDate();
+            todayHour = today.getHours();
+            todayMinute = today.getMinutes();
+            todaySecond = today.getSeconds();
+            t1 = c.loveTime;
+            t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
+            diff = t2 - t1;
+            diffYears = Math.floor(diff / years);
+            diffDays = Math.floor(diff / days - diffYears * 365);
+            diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
+            diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
+            diffSeconds = Math.floor(
                 (diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds
             );
-            this.timeText =
-                "我们已经在一起 " +
-                diffYears +
-                "年" +
-                diffDays +
-                "天" +
-                diffHours +
-                "小时" +
-                diffMinutes +
-                "分钟" +
-                diffSeconds +
-                "秒啦";
-        }
+            this.timeText = `我们已经在一起 ${diffYears} 年 ${diffDays} 天 ${diffHours} 小时 ${diffMinutes} 分钟 ${diffSeconds} 秒啦！`;
+        },
+
+        /**
+         * 获取 Gitee 仓库信息
+         */
+        getGiteeData() {
+            axios.get(`https://gitee.com/api/v5/users/${c.giteeName}/repos?type=personal&sort=updated&page=1&per_page=100`).then(res => {
+                this.giteeData = res.data;
+            });
+        },
     }
 });
---
+++ NutssssIndex2/js/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: N0ts
+ * @Date: 2021-12-03 09:57:35
+ * @LastEditTime: 2021-12-03 10:12:30
+ * @Description: 配置
+ * @FilePath: /NutssssIndex2/js/config.js
+ * @Mail：mail@n0ts.cn
+ */
+
+export default {
+    loveTime: Date.UTC(2018, 11, 19), // 恋爱时间
+    giteeName: "N0ts" // Gitee 账号名
+};
